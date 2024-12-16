179fcf089c05e031f7c2d2db397b96d01112e596
23-faisal <a.faisal23r@gmail.com>
2024-12-16 10:37:09 +0600

    Create get all the notes functionality in the backend

--- backend/controllers/notes.controller.js
+++ backend/controllers/notes.controller.js
@@ -2,6 +2,8 @@ import errorHandler from "../utils/error.js";
 import Notes from "../models/note.model.js";
 import Note from "../models/note.model.js";
 
+// add note
+
 export const addNote = async (req, res, next) => {
   try {
     const { title, content, tags } = req.body;
@@ -39,6 +41,8 @@ export const addNote = async (req, res, next) => {
   }
 };
 
+// edit note
+
 export const editNote = async (req, res, next) => {
   try {
     const { noteId } = req.params;
@@ -72,6 +76,31 @@ export const editNote = async (req, res, next) => {
       success: false,
       message: "Note updated successfully",
       note,
+    });
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
+
+// get all the note s
+
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+
+    const notes = await Notes.find({ userId: _id }).sort({ isPinned: -1 });
+
+    if (!notes) {
+      return res.status(400).json({
+        success: false,
+        message: "No notes found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      notes,
+      message: "All notes retreived successfully",
     });
   } catch (error) {
     next(errorHandler(error));
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { addNote, editNote } from "../controllers/notes.controller.js";
+import {
+  addNote,
+  editNote,
+  getAllNotes,
+} from "../controllers/notes.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const notesRouter = Router();
@@ -7,5 +11,7 @@ const notesRouter = Router();
 notesRouter.post("/add-note", authMiddleware, addNote);
 
 notesRouter.put("/edit-note/:noteId", authMiddleware, editNote);
+
+notesRouter.get("/all-notes", authMiddleware, getAllNotes);
 
 export default notesRouter;
