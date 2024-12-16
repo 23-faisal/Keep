5be768ce96da495ea747067c986735ff80d9d435
23-faisal <a.faisal23r@gmail.com>
2024-12-16 11:20:13 +0600

    Create  'update pinned value of a note' functionality in the backend

--- backend/controllers/notes.controller.js
+++ backend/controllers/notes.controller.js
@@ -107,6 +107,8 @@ export const getAllNotes = async (req, res, next) => {
   }
 };
 
+// delete a note
+
 export const deleteNote = async (req, res, next) => {
   try {
     const { _id } = req.user;
@@ -132,6 +134,40 @@ export const deleteNote = async (req, res, next) => {
     res.status(200).json({
       success: true,
       message: "Note deleted successfully",
+    });
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
+
+// update pinned value of a note
+
+export const updatePinnedValue = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const { _id } = req.user;
+
+    const note = await Note.findOne({
+      _id: noteId,
+      userId: _id,
+    });
+
+    if (!note) {
+      return res.status(400).json({
+        success: false,
+        message: "Note not found",
+      });
+    }
+
+    // Toggle the isPinned value
+    note.isPinned = !note.isPinned;
+
+    await note.save();
+
+    res.status(200).json({
+      success: true,
+      message: `Note ${note.isPinned ? "pinned" : "unpinned"} successfully`,
+      note,
     });
   } catch (error) {
     next(errorHandler(error));
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -4,17 +4,29 @@ import {
   deleteNote,
   editNote,
   getAllNotes,
+  updatePinnedValue,
 } from "../controllers/notes.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const notesRouter = Router();
 
+// add note
 notesRouter.post("/add-note", authMiddleware, addNote);
 
+// edit note
 notesRouter.put("/edit-note/:noteId", authMiddleware, editNote);
 
+// get all the notes
 notesRouter.get("/all-notes", authMiddleware, getAllNotes);
 
+// delete a note
 notesRouter.delete("/delete-note/:noteId", authMiddleware, deleteNote);
+
+// update pinned value of a note
+notesRouter.put(
+  "/update-note-pinned/:noteId",
+  authMiddleware,
+  updatePinnedValue
+);
 
 export default notesRouter;
