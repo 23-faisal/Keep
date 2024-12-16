25a16e1fe51d1c90291244bb3fe0e0a652c6cad2
23-faisal <a.faisal23r@gmail.com>
2024-12-16 10:28:27 +0600

    create edit note functionality in the backend

--- backend/controllers/notes.controller.js
+++ backend/controllers/notes.controller.js
@@ -1,5 +1,6 @@
 import errorHandler from "../utils/error.js";
 import Notes from "../models/note.model.js";
+import Note from "../models/note.model.js";
 
 export const addNote = async (req, res, next) => {
   try {
@@ -32,6 +33,45 @@ export const addNote = async (req, res, next) => {
       success: true,
       message: "Note added successfully!",
       note: newNote,
+    });
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
+
+export const editNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const { title, content, tags, isPinned } = req.body;
+    const { _id } = req.user;
+
+    if (!title && !content && !tags) {
+      return res.status(400).json({
+        success: false,
+        message: "No changes provided!",
+      });
+    }
+
+    const note = await Note.findOne({ _id: noteId, userId: _id });
+
+    if (!note) {
+      return res.status(400).json({
+        success: false,
+        message: "Note not found!",
+      });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    res.status(200).json({
+      success: false,
+      message: "Note updated successfully",
+      note,
     });
   } catch (error) {
     next(errorHandler(error));
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addNote } from "../controllers/notes.controller.js";
+import { addNote, editNote } from "../controllers/notes.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const notesRouter = Router();
 
 notesRouter.post("/add-note", authMiddleware, addNote);
+
+notesRouter.put("/edit-note/:noteId", authMiddleware, editNote);
 
 export default notesRouter;
